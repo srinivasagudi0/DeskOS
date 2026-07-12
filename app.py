e1d279b904fb6e228154d803a6a9b662087b5e68
Srinivasa Gudi <239487102+srinivasagudi0@users.noreply.github.com>
2026-07-12 15:05:37 -0500

    add css styling for heaader so i can get it in the center of the page and rendereded it

--- app.py
+++ app.py
@@ -1,3 +1,27 @@
+from pathlib import Path
+
 import streamlit as st
 
+
+def render_css():
+    css_path = Path(__file__).with_name("app.css")
+    with css_path.open() as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
+render_css()
+
+
 st.title("DeskOS")
+
+st.markdown("""
+    <br />
+    <hr />
+            """, unsafe_allow_html=True)
+
+left, middle, right = st.columns(3)
+
+with left: 
+    container = st.container(border=True)
+    with container:
+        st.write("This is the left column.")
